e0a5cab26b96b181c5a7fcf3fe5be698e2e59aef
Amr69130 <amrouchenorris@gmail.com>
2025-08-04 16:34:26 +0200

    Add user profile route and template with authentication check

---
+++ src/Controller/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_user_profile')]
+    public function profile(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour voir cette page.');
+        }
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
