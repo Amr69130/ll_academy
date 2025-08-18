77bc4b98a197d2b2a97d68a96a5951920b268cee
Yukamiro <yukamiro2@gmail.com>
2025-08-18 16:55:19 +0200

    ajout du formulaire d'update d'information du User (tel, adress, code postal...) dans les paramètres du User

--- migrations/Version20250818132038.php
+++ migrations/Version20250818132038.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250812100746 extends AbstractMigration
+final class Version20250818132038 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -27,7 +27,7 @@ final class Version20250812100746 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE student CHANGE address adress VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user CHANGE billing_address billing_adress VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE student CHANGE address adress VARCHAR(255) NOT NULL');
     }
 }
