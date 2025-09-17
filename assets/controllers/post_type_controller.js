523497b51133532d57ee02ec50e1443df1a52482
Yukamiro <yukamiro2@gmail.com>
2025-09-17 12:12:10 +0200

    debug twig

--- assets/controllers/post_type_controller.js
+++ assets/controllers/post_type_controller.js
@@ -4,25 +4,25 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
 
     static targets = ["row"];
-   
 
-    post(event){
+
+    post(event) {
         console.log(event.target.index);
         console.log(event.type);
         console.log(this.targets.dataset);
         console.log(event.target.value);
-        
+
         let post = event.target.value;
 
         this.rowTargets.forEach(row => {
             let rowType = row.dataset.typeId
             console.log("Type de l'id", rowType)
-            
-            if(post === rowType || !post){
+
+            if (post === rowType || !post) {
                 row.style.display = "";
-            } else{
+            } else {
                 row.style.display = "none";
             }
         });
-        }
+    }
 }
